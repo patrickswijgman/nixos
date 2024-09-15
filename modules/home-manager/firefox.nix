8a8488ad403cfe88d86eec11dad61fc8b612e38f
Patrick <petrik09@live.nl>
2024-09-15 11:17:00 +0200

    feat: firefox settings

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -3,6 +3,30 @@
 {
   programs.firefox = {
     enable = true;
+    package = pkgs.firefox-esr;
+    languagePacks = [
+      "en-US"
+      "nl"
+    ];
+    policies = {
+      RequestedLocales = [
+        "en-US"
+        "nl"
+      ];
+      SearchEngines.Default = "DuckDuckGo";
+      DisablePocket = true;
+      DisplayBookmarksToolbar = "never";
+      DisplayMenuBar = "default-off";
+      FirefoxHome.TopSites = false;
+      FirefoxHome.SponsoredTopSites = false;
+      FirefoxSuggest.WebSuggestions = false;
+      FirefoxSuggest.SponsoredSuggestions = false;
+      TranslateEnabled = false;
+      Preferences = {
+        "browser.aboutConfig.showWarning" = false;
+        "browser.ctrlTab.sortByRecentlyUsed" = true;
+      };
+    };
   };
 
   home.sessionVariables = {
