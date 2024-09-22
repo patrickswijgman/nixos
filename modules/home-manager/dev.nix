2ea981da3cbc4dea369fbf5c08e467c9b268b744
Patrick <patrick.swijgman@wearespindle.com>
2024-09-22 11:57:40 +0200

    feat: more language servers

--- modules/home-manager/dev.nix
+++ modules/home-manager/dev.nix
@@ -13,8 +13,16 @@
     golangci-lint
     golangci-lint-langserver
 
+    nodejs_20
+
     typescript-language-server
     vscode-langservers-extracted
     tailwindcss-language-server
+
+    marksman
+    yaml-language-server
+
+    docker-compose-language-service
+    dockerfile-language-server-nodejs
   ];
 }
