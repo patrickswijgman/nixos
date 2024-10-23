c680a79071ad98b555550c9e56fb18d595dbb0f4
Patrick <petrik09@live.nl>
2024-10-23 19:54:58 +0200

    feat: zed editor

--- modules/dev.nix
+++ modules/dev.nix
@@ -1,9 +1,5 @@
 { config, pkgs, ... }:
 
-let
-  scls = pkgs.callPackage ./custom/simple-completion-language-server.nix { };
-  vlse = pkgs.callPackage ./custom/vscode-langservers-extracted.nix { };
-in
 {
   home-manager.users.patrick = {
     programs.go = {
@@ -12,40 +8,20 @@ in
 
     home.packages = with pkgs; [
       # Nix
-      nil
+      nixd
       nixfmt-rfc-style
 
       # Go
       gopls
       golangci-lint
-      golangci-lint-langserver
 
       # Rust
       rustc
       cargo
 
-      # Python
-      pyright
-      ruff
-
       # Web
       nodejs_20
-      nodePackages.prettier
-      typescript-language-server
-      tailwindcss-language-server
-      (vlse)
       deno
-
-      # Text and config files
-      marksman
-      yaml-language-server
-      taplo
-      typos-lsp
-      (scls)
-
-      # Docker
-      docker-compose-language-service
-      dockerfile-language-server-nodejs
     ];
   };
 }
--- modules/editor.nix
+++ modules/editor.nix
@@ -1,263 +1,77 @@
 { config, pkgs, ... }:
 
-let
-  helix = pkgs.callPackage ./custom/helix.nix { };
-in
 {
   home-manager.users.patrick = {
-    programs.helix = {
+    programs.zed-editor = {
       enable = true;
-      package = helix;
-      settings = {
-        theme = "autumn";
-
-        # https://github.com/helix-editor/helix/blob/master/book/src/editor.md
-        editor = {
-          scrolloff = 9999;
-          cursorline = true;
-          line-number = "relative";
-          bufferline = "multiple";
-          popup-border = "all";
-          shell = [
-            "fish"
-            "-c"
-          ];
-          end-of-line-diagnostics = "hint";
-          inline-diagnostics = {
-            cursor-line = "warning";
-          };
-          cursor-shape = {
-            normal = "block";
-            insert = "bar";
-            select = "underline";
-          };
-          file-picker = {
-            hidden = false;
-          };
-          lsp = {
-            display-messages = true;
-          };
-          soft-wrap = {
-            enable = true;
-          };
+      # https://zed.dev/docs/configuring-zed#settings
+      userSettings = {
+        theme = "Fleet Dark";
+        auto_update = false;
+        vim_mode = true;
+        tab_bar = {
+          show = true;
+          show_nav_history_buttons = false;
         };
-
-        keys = {
-          normal = {
-            "C-h" = "jump_view_left";
-            "C-j" = "jump_view_down";
-            "C-k" = "jump_view_up";
-            "C-l" = "jump_view_right";
-            "C-r" = "redo";
-            "A-h" = "goto_previous_buffer";
-            "A-l" = "goto_next_buffer";
-            "A-q" = ":buffer-close";
-            "A-tab" = "goto_last_accessed_file";
-            "q" = "no_op";
-            "Q" = "no_op";
-            "x" = "select_line_below";
-            "X" = "select_line_above";
-            "Y" = [
-              "extend_to_line_bounds"
-              "yank"
-            ];
-            "D" = [
-              "extend_to_line_bounds"
-              "delete_selection"
-            ];
-            "esc" = [
-              "collapse_selection"
-              "keep_primary_selection"
-            ];
-          };
-          insert = {
-            "esc" = [
-              "collapse_selection"
-              "normal_mode"
-            ];
-          };
-          select = {
-            "C-d" = [
-              "search_selection"
-              "extend_search_next"
-            ];
-            "esc" = [
-              "collapse_selection"
-              "keep_primary_selection"
-              "normal_mode"
-            ];
-          };
+        tabs = {
+          close_position = "right";
+          file_icons = false;
+          git_status = false;
+          activate_on_close = "history";
         };
-      };
-
-      # https://github.com/helix-editor/helix/blob/master/book/src/languages.md
-      languages = {
-        language-server = {
-          scls = {
-            command = "simple-completion-language-server";
-          };
-          typos = {
-            command = "typos-lsp";
-          };
-          typescript-language-server = {
-            config.preferences.importModuleSpecifierPreference = "non-relative";
-            config.preferences.importModuleSpecifierEnding = "js";
-          };
-        };
-        language = [
-          {
-            name = "nix";
-            auto-format = true;
-            formatter.command = "nixfmt";
-          }
-          {
-            name = "python";
-            language-servers = [
-              "pyright"
-              "ruff"
-              "typos"
-            ];
-            auto-format = true;
-          }
-          {
-            name = "typescript";
-            language-servers = [
-              "typescript-language-server"
-              "vscode-eslint-language-server"
-              "typos"
-            ];
-            auto-format = true;
-            formatter = {
-              command = "prettier";
-              args = [
-                "--parser"
-                "typescript"
-              ];
-            };
-          }
-          {
-            name = "tsx";
-            language-servers = [
-              "typescript-language-server"
-              "vscode-eslint-language-server"
-              "tailwindcss-ls"
-              "typos"
-            ];
-            auto-format = true;
-            formatter = {
-              command = "prettier";
-              args = [
-                "--parser"
-                "typescript"
-              ];
-            };
-          }
-          {
-            name = "javascript";
-            language-servers = [
-              "typescript-language-server"
-              "vscode-eslint-language-server"
-              "typos"
-            ];
-            auto-format = true;
-            formatter = {
-              command = "prettier";
-              args = [
-                "--parser"
-                "typescript"
-              ];
-            };
-          }
-          {
-            name = "jsx";
-            language-servers = [
-              "typescript-language-server"
-              "vscode-eslint-language-server"
-              "tailwindcss-ls"
-              "typos"
-            ];
-            auto-format = true;
-            formatter = {
-              command = "prettier";
-              args = [
-                "--parser"
-                "typescript"
-              ];
-            };
-          }
-          {
-            name = "html";
-            auto-format = true;
-            formatter = {
-              command = "prettier";
-              args = [
-                "--parser"
-                "html"
-              ];
-            };
-          }
-          {
-            name = "css";
-            auto-format = true;
-            formatter = {
-              command = "prettier";
-              args = [
-                "--parser"
-                "css"
-              ];
-            };
-          }
-          {
-            name = "json";
-            auto-format = true;
-            formatter = {
-              command = "prettier";
-              args = [
-                "--parser"
-                "json"
-              ];
-            };
-          }
-          {
-            name = "yaml";
-            auto-format = true;
-            formatter = {
-              command = "prettier";
-              args = [
-                "--parser"
-                "yaml"
-              ];
-            };
-          }
-          {
-            name = "markdown";
-            language-servers = [
-              "marksman"
-              "typos"
-              "scls"
-            ];
-            auto-format = true;
-            formatter = {
-              command = "prettier";
-              args = [
-                "--parser"
-                "markdown"
-              ];
-            };
-          }
+        soft_wrap = "editor_width";
+        wrap_guides = [
+          80
+          120
         ];
+        terminal = {
+          font_family = "FiraCode Nerd Font";
+        };
+        languages = {
+          Nix = {
+            language_servers = [ "nil" ];
+          };
+          HTML = {
+            formatter = "prettier";
+            format_on_save = "on";
+          };
+          Markdown = {
+            formatter = "prettier";
+            format_on_save = "on";
+          };
+        };
+        lsp = {
+          vtsls = {
+            settings = {
+              typescript = {
+                preferences = {
+                  importModuleSpecifier = "non-relative";
+                  importModuleSpecifierEnding = "js";
+                };
+              };
+            };
+          };
+        };
       };
-
-      # https://github.com/helix-editor/helix/blob/master/book/src/themes.md#syntax-highlighting
-      themes = { };
+      # https://zed.dev/docs/key-bindings
+      userKeymaps = { };
+      # https://github.com/zed-industries/extensions/tree/main/extensions
+      extensions = [
+        "fleet-themes"
+        "nix"
+        "dockerfile"
+        "docker-compose"
+        "html"
+        "deno"
+        "ruff"
+      ];
     };
-
-    home.packages = with pkgs; [
-      gcc
-    ];
 
     home.sessionVariables = {
-      EDITOR = "hx";
+      EDITOR = "zeditor";
     };
   };
+
+  # Enable dynamic execution of binaries, e.g. Node to install language servers.
+  programs.nix-ld.enable = true;
 }
