f521bf2dc65ebda01f371c21796fdaf48e91f03d
Patrick <patrick.swijgman@wearespindle.com>
2024-11-21 08:46:30 +0100

    feat: nvim config fixes and better searching

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -56,40 +56,27 @@ vim.g.mapleader = " "
 -- KEYMAPS
 
 local function find()
-	local dir = vim.fn.expand("%:h") .. "/"
-	utils.with_input("Find: ", dir, "customlist,v:lua.require'utils'.list_files", function(input)
+	utils.with_input("Find: ", "", "customlist,v:lua.require'utils'.list_files", function(input)
 		vim.cmd("silent find " .. input)
 	end)
 end
 
 local function grep()
 	local word = vim.fn.expand("<cword>")
-	utils.with_input("Grep: ", word, nil, function(input)
+	utils.with_input("Grep: ", word, "customlist,v:lua.require'utils'.list_words", function(input)
 		vim.cmd("silent grep! " .. input .. " | bo copen")
+	end)
+end
+
+local function buffers()
+	utils.with_input("Buffer: ", "", "buffer", function(input)
+		vim.cmd("silent buffers " .. input)
 	end)
 end
 
 local function help()
 	local word = vim.fn.expand("<cword>")
-	vim.cmd("silent help " .. word .. "<cr>")
-end
-
-local function delete()
-	local file = vim.fn.expand("%")
-	utils.with_confirm("Delete: " .. file, function()
-		vim.cmd("silent !rm -r " .. file)
-		utils.delete_buf_with_filename(file)
-	end)
-end
-
-local function move()
-	local file = vim.fn.expand("%")
-	local dir = vim.fn.expand("%:h")
-	utils.with_input("Move: ", file, "file", function(input)
-		vim.cmd("silent !mkdir -p " .. dir)
-		vim.cmd("silent !mv " .. file .. " " .. input)
-		utils.delete_buf_with_filename(file)
-	end)
+	vim.cmd("silent help " .. word .. " <cr>")
 end
 
 local function create()
@@ -104,52 +91,73 @@ local function create()
 	end)
 end
 
+local function move()
+	local file = vim.fn.expand("%")
+	utils.with_input("Move: ", file, "file", function(input)
+		vim.cmd("silent !mkdir -p " .. utils.dirname(input))
+		vim.cmd("silent !mv " .. file .. " " .. input)
+		vim.cmd("edit " .. input)
+		utils.delete_buf_with_filename(file)
+	end)
+end
+
+local function delete()
+	local file = vim.fn.expand("%")
+	utils.with_confirm("Delete: " .. file, function()
+		vim.cmd("silent !rm -r " .. file)
+		utils.delete_buf_with_filename(file)
+	end)
+end
+
 local function tree(dir)
-	vim.cmd("silent !tree --gitignore " .. dir)
+	local output = vim.fn.system("tree --gitignore " .. (dir or ""))
+	vim.notify(output, vim.log.levels.INFO, { title = "Tree" })
 end
 
 local function tree_current_file_dir()
 	tree(vim.fn.expand("%:h"))
 end
 
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
-vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
+local opts = { noremap = true, silent = true }
 
-vim.keymap.set("n", "<leader>e", tree_current_file_dir)
-vim.keymap.set("n", "<leader>E", tree)
-vim.keymap.set("n", "<leader>f", find)
-vim.keymap.set("n", "<leader>g", grep)
-vim.keymap.set("n", "<leader>h", help)
-vim.keymap.set("n", "<leader>c", create)
-vim.keymap.set("n", "<leader>m", move)
-vim.keymap.set("n", "<leader>D", delete)
-vim.keymap.set("n", "<leader>i", ":Inspect<cr>")
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', opts)
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', opts)
 
-vim.keymap.set("n", "<leader>q", ":copen<cr>")
-vim.keymap.set("n", "]q", ":cnext<cr>")
-vim.keymap.set("n", "[q", ":cprev<cr>")
+vim.keymap.set("n", "<leader>e", tree_current_file_dir, opts)
+vim.keymap.set("n", "<leader>E", tree, opts)
+vim.keymap.set("n", "<leader>f", find, opts)
+vim.keymap.set("n", "<leader>g", grep, opts)
+vim.keymap.set("n", "<leader>h", help, opts)
+vim.keymap.set("n", "<leader>c", create, opts)
+vim.keymap.set("n", "<leader>m", move, opts)
+vim.keymap.set("n", "<leader>D", delete, opts)
+vim.keymap.set("n", "<leader>i", ":Inspect<cr>", opts)
 
-vim.keymap.set("n", "<leader>b", ":buffers ")
-vim.keymap.set("n", "]b", ":bnext<cr>")
-vim.keymap.set("n", "[b", ":bprev<cr>")
+vim.keymap.set("n", "<leader>q", ":copen<cr>", opts)
+vim.keymap.set("n", "]q", ":cnext<cr>", opts)
+vim.keymap.set("n", "[q", ":cprev<cr>", opts)
 
-vim.keymap.set("n", "<leader>t", ":tabnew<cr>")
-vim.keymap.set("n", "]t", ":tabnext<cr>")
-vim.keymap.set("n", "[t", ":tabprev<cr>")
-vim.keymap.set("n", "<a-h>", ":tabprev<cr>")
-vim.keymap.set("n", "<a-l>", ":tabnext<cr>")
-vim.keymap.set("n", "<a-q>", ":tabclose<cr>")
-vim.keymap.set("n", "<a-tab>", ":tablast<cr>")
+vim.keymap.set("n", "<leader>b", buffers, opts)
+vim.keymap.set("n", "]b", ":bnext<cr>", opts)
+vim.keymap.set("n", "[b", ":bprev<cr>", opts)
 
-vim.keymap.set("n", "<c-h>", "<c-w>h")
-vim.keymap.set("n", "<c-j>", "<c-w>j")
-vim.keymap.set("n", "<c-k>", "<c-w>k")
-vim.keymap.set("n", "<c-l>", "<c-w>l")
-vim.keymap.set("n", "<c-q>", "<c-w>q")
-vim.keymap.set("n", "<c-tab>", "<c-w>w")
+vim.keymap.set("n", "<leader>t", ":tabnew<cr>", opts)
+vim.keymap.set("n", "]t", ":tabnext<cr>", opts)
+vim.keymap.set("n", "[t", ":tabprev<cr>", opts)
+vim.keymap.set("n", "<a-h>", ":tabprev<cr>", opts)
+vim.keymap.set("n", "<a-l>", ":tabnext<cr>", opts)
+vim.keymap.set("n", "<a-q>", ":tabclose<cr>", opts)
+vim.keymap.set("n", "<a-tab>", ":tablast<cr>", opts)
 
-vim.keymap.set({ "n", "v" }, "q", "")
-vim.keymap.set({ "n", "v" }, "Q", "")
+vim.keymap.set("n", "<c-h>", "<c-w>h", opts)
+vim.keymap.set("n", "<c-j>", "<c-w>j", opts)
+vim.keymap.set("n", "<c-k>", "<c-w>k", opts)
+vim.keymap.set("n", "<c-l>", "<c-w>l", opts)
+vim.keymap.set("n", "<c-q>", "<c-w>q", opts)
+vim.keymap.set("n", "<c-tab>", "<c-w>w", opts)
+
+vim.keymap.set({ "n", "v" }, "q", "", opts)
+vim.keymap.set({ "n", "v" }, "Q", "", opts)
 
 -- PLUGINS
 
--- files/nvim/lua/utils.lua
+++ files/nvim/lua/utils.lua
@@ -20,6 +20,33 @@ function M.list_files(query)
 	return vim.fn.globpath("**", query .. "*", false, true)
 end
 
+function M.list_words(starts_with)
+	local bufs = vim.api.nvim_list_bufs()
+	local words_list = {}
+	local words_set = {}
+
+	for _, buf in ipairs(bufs) do
+		if vim.api.nvim_buf_is_loaded(buf) then
+			local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+
+			for _, line in ipairs(lines) do
+				for word in line:gmatch("%a+") do
+					if not words_set[word] then
+						words_set[word] = true
+						if word:find("^" .. starts_with) then
+							words_list[#words_list + 1] = word
+						end
+					end
+				end
+			end
+		end
+	end
+
+	table.sort(words_list)
+
+	return words_list
+end
+
 function M.ends_with(str, tail)
 	return str:match(tail .. "$")
 end
@@ -34,7 +61,7 @@ function M.delete_buf_with_filename(filename)
 	for _, buf in ipairs(bufs) do
 		local name = vim.api.nvim_buf_get_name(buf)
 
-		if name:match("^" .. filename) then
+		if name:match(filename) then
 			vim.api.nvim_buf_delete(buf, { force = true })
 		end
 	end
